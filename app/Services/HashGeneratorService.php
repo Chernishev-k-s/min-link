67297ac891ceff5d88b771ec098c060f5be54e20
k.s.chernishev@gmail.com <k.s.chernishev@gmail.com>
2021-11-04 03:30:13 +0200

    Add hash generator

---
+++ app/Services/HashGeneratorService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services;
+
+class HashGeneratorService
+{
+    public static function encode(int $int, int $length_min = 5, array $tokens = []): string
+    {
+        $tokens = $tokens ?: config('hash.tokens');
+        $tokens_count = count($tokens);
+
+        $hash = '';
+        while ($int != 0)
+        {
+            $hash = $tokens[floor(abs($int % $tokens_count))] . $hash;
+            $int = floor($int / $tokens_count);
+        }
+        return str_pad($hash, $length_min, $tokens[0], STR_PAD_LEFT);
+    }
+
+    public static function decode(string $hash, array $tokens = []): int
+    {
+        $tokens = array_flip($tokens ?: config('hash.tokens'));
+        $tokens_count = count($tokens);
+
+        $arr = [];
+        foreach (str_split($hash) as $i => $token)
+        {
+            if(!isset($tokens[$token])) {
+                throw new \Exception("Incorrect combination of hash and tokens. Found unknown token - $token");
+            }
+
+            $arr[] = $tokens[$token];
+        }
+
+        $int = 0;
+        foreach (array_reverse($arr) as $i => $val)
+        {
+            $int += $val * ($tokens_count ** $i);
+        }
+        return $int;
+    }
+}
